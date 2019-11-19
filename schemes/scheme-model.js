bf9ab2dad1c98782f4f9b83c32aeca60a7433448
Kyle Barnett <kbarnett23@gmail.com>
2019-11-19 09:29:20 -0700

    findsteps working with join statement

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,22 @@
 const db = require('../data/db-config');
 
 module.exports = {
-  find
+  find,
+  findById,
+  findSteps
 }
 
 function find() {
   return db('schemes')
+}
+
+function findById(id) {
+  return db('schemes').where({ id })
+}
+
+function findSteps(id) {
+  return db('schemes as sc')
+    .join('steps as s', 'sc.id', 's.id')
+    .select('sc.id', 'scheme_name', 's.step_number', 's.instructions')
+    .where({ scheme_id: id })
 }
